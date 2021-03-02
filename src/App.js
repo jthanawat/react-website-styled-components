6a3bb591872f5da843527e77f99414e23c6bff0b
jthanawat <kla13thanawat@gmail.com>
2021-03-02 16:27:12 +0700

    add HomePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import GlobalStyles from './globalStyles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/HomePage/Home';
 import { Navbar } from './components';
 
 function App() {
   return (
     <Router>
+      <GlobalStyles />
       <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
     </Router>
   );
 }
