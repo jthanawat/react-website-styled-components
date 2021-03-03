bd21a10d60a632c5adaf79f81de1ae73ea311bed
jthanawat <kla13thanawat@gmail.com>
2021-03-03 16:08:45 +0700

    add ScrollToTop()

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import GlobalStyles from './globalStyles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/HomePage/Home';
 import { Navbar, Footer } from './components';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
       <GlobalStyles />
+      <ScrollToTop />
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
